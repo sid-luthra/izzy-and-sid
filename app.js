468398a8987d114a977971f5babe592146383def
Sid Luthra <sidluthra@gmail.com>
2022-03-15 22:53:39 -0400

    Added files and changes required for heroku deploy

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var app = express();
 
 // mongoose setup
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://siteadmin:Wedd111095@sandbox.pju4d.mongodb.net/wedding?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://siteadmin:Wedd111095@sandbox.pju4d.mongodb.net/wedding?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
