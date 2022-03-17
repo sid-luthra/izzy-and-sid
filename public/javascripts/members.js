d8acd3ac7a28760698b554156b8a9d79b05dcf5a
Sid Luthra <sidluthra@gmail.com>
2022-03-17 12:25:44 -0400

    Dynamic number of members on add / remove

--- public/javascripts/members.js
+++ public/javascripts/members.js
@@ -3,6 +3,14 @@ const renumberMembers = () => {
   let count = 1;
   members.forEach((member) => {
     member.id = `member${count}`;
+
+    const inputs = member.querySelectorAll('.member-input');
+    inputs.forEach((input) => {
+      const newName = input.name.substr(0, input.name.length - 1);
+      input.name = `${newName}${count}`;
+      input.id = input.name;
+    });
+
     count += 1;
   });
 };
@@ -28,6 +36,12 @@ const addMember = () => {
   const newMember = template.cloneNode(true);
   newMember.id = `member${newMemberCount}`;
   newMember.classList.remove('hidden');
+
+  const inputs = newMember.querySelectorAll('.member-input');
+  inputs.forEach((input) => {
+    input.name = `${input.name}${newMemberCount}`;
+    input.id = input.name;
+  });
 
   const delBtn = document.createElement('button');
   delBtn.classList.add('text-button');
