17bc95bbb2afad933c3ed60f4fc1ca05a1119240
Sid Luthra <sidluthra@gmail.com>
2022-03-17 22:31:07 -0400

    Compression and helmet

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
 
 // mongoose setup
 const mongoose = require('mongoose');
@@ -28,6 +30,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
